424f2ca1f3d6e02d12eac23ee7810c7ac5f97b5a
JhayNhet <opedaps1@gmail.com>
2022-09-15 19:24:20 -0700

    basic tasks

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,25 +1,12 @@
-#include "holberton.h"
-
-
-
+#include "main.h"
 /**
-
  * mul - multiplies two integers
-
  * @a: one integer
-
  * @b: second integer
-
  * Return: product of a and b
-
  */
 
-
-
 int mul(int a, int b)
-
 {
-
 	return (a * b);
-
 }
